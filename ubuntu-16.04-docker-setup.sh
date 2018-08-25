be1eb5682cb0d38a00c30fb23a5e3f6bd0b477fb
zibastian-mn <42696330+zibastian-mn@users.noreply.github.com>
2018-08-25 17:52:12 +0200

    Update ubuntu-16.04-docker-setup.sh

--- ubuntu-16.04-docker-setup.sh
+++ ubuntu-16.04-docker-setup.sh
@@ -12,7 +12,7 @@ do
   echo -e "Enter your selection: \c"
   read answer
   case "$answer" in
-    1) bash -c "$(wget -O - https://raw.githubusercontent.com/buzzkillb/EGEM-node-install/master/createswap.sh)" && bash -c "$(wget -O - https://raw.githubusercontent.com/zibastian-mn/egem-quarry-node-docker/master/ubuntu-16.04-docker-setup.sh)" && exit ;;
+    1) bash -c "$(wget -O - https://raw.githubusercontent.com/buzzkillb/EGEM-node-install/master/createswap.sh)" && bash -c "$(wget -O - https://raw.githubusercontent.com/zibastian-mn/egem-quarry-node-docker/master/ubuntu-16.04-docker-deploy.sh)" && exit ;;
     2) bash -c "$(wget -O - https://raw.githubusercontent.com/zibastian-mn/egem-quarry-node-docker/master/ubuntu-16.04-docker-deploy.sh)" && exit ;;
     q) exit ;;
   esac
