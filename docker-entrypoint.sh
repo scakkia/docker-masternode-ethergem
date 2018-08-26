f0d6497d92e76f5b58f53fa3812453fd790d8379
zibastian-mn <42696330+zibastian-mn@users.noreply.github.com>
2018-08-26 20:49:08 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,13 +18,14 @@ cd /opt/egem/egem-net-intelligence-api && pm2 start app.json
 
 MEMORY=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 
-if [ $MEMORY -lt 3000000 ]; then
-	CACHE='--cache 512'
-fi
-
 if [ $MEMORY -lt 2000000 ]; then
 	echo "The host system has less than 2 Gb of RAM. Starting with --cache 256"
 	CACHE='--cache 256'
+else
+	if [ $MEMORY -lt 4000000 ]; then
+		echo "The host system has less than 4 Gb of RAM. Starting with --cache 512"
+		CACHE='--cache 512'
+	fi
 fi
 
 egem --datadir /opt/egem/live-net/ --maxpeers 100 --rpc $CACHE
